4ea1dad5edaea2f796f23095fb02766987c2a965
Angie Munoz <angiepmunoz@gmail.com>
2024-02-26 14:33:57 -0500

    completed animes crud routes

--- controllers/animes.js
+++ controllers/animes.js
@@ -1,52 +1,90 @@
-const express = require("express")
+const express = require("express");
 const animes = express.Router();
-const animesArr = require("../data/animes")
-const { getAllAnimes } = require("../queries/animes.js")
+const animesArr = require("../data/animes");
+const {
+  getAllAnimes,
+  getOneAnime,
+  createAnime,
+  updateOneAnime,
+  deleteOneAnime
+} = require("../queries/animes.js");
+
+// validation
+function validateAnime(req, res, next) {
+  const body = req.body;
+  if (!body.name || !body.description) {
+    res.status(400).json({payload: "Please make sure to include a Name and a Description"});
+  } else {
+    next();
+  }
+}
 
 // GET all resources INDEX
 animes.get("/", async (req, res) => {
-    try{
-        const allAnimes = await getAllAnimes();
-        res.status(200).json({payload: allAnimes})
-    }catch(error){
-        res.status(404).json({payload: error})
-    }
-    
-})
+  try {
+    const allAnimes = await getAllAnimes();
+    res.status(200).json({ payload: allAnimes });
+  } catch (error) {
+    res.status(404).json({ payload: error });
+  }
+});
 
 // GET one resource SHOW
-animes.get("/:animeId", (req, res) => {
-    const {animeId} = req.params;
-    const foundAnime = animesArr.filter(anime => anime.id == animeId)
-    if(foundAnime.length){
-        res.status(200).json({payload: foundAnime[0]})
-    }else {
-        res.status(404).json({payload: "anime not found"})
-    }
-})
+animes.get("/:animeId", async (req, res) => {
+  const { animeId } = req.params;
+  try {
+    const anime = await getOneAnime(animeId);
+    res.status(200).json({ payload: anime });
+  } catch (error) {
+    res.status(404).json({ payload: error });
+  }
+  // const foundAnime = animesArr.filter(anime => anime.id == animeId)
+  // if(foundAnime.length){
+  //     res.status(200).json({payload: foundAnime[0]})
+  // }else {
+  //     res.status(404).json({payload: "anime not found"})
+  // }
+});
 
-// CREATE/POST a resource 
-animes.post("/", (req, res)=>{
-    const newAnime = req.body;
-    animesArr.push(newAnime)
-    res.status(201).json({payload: animesArr[animesArr.length-1]})
-})
+// CREATE/POST a resource
+animes.post("/", validateAnime, async (req, res) => {
+  const body = req.body;
+  try {
+    const newAnime = await createAnime(body);
+    res.status(201).json({ payload: newAnime });
+  } catch (error) {
+    res.status(404).json({ payload: error });
+  }
+  // animesArr.push(newAnime)
+  // res.status(201).json({payload: animesArr[animesArr.length-1]})
+});
 
 // UPDATE/PUT a resource
-animes.put("/:animeId", (req, res)=>{
-    const {animeId} = req.params;
-    const updatedAnime = req.body;
-    animesArr.splice(animeId-1,1,updatedAnime);
-    res.status(201).json({payload: animesArr[animeId-1]})
-})
+animes.put("/:animeId", validateAnime, async (req, res) => {
+  const { animeId } = req.params;
+  const body = req.body;
+  try {
+    const updatedAnime = await updateOneAnime(animeId, body);
+    res.status(201).json({ payload: updatedAnime });
+  } catch (error) {
+    res.status(404).json({ payload: error });
+  }
 
+  // animesArr.splice(animeId-1,1,updatedAnime);
+  // res.status(201).json({payload: animesArr[animeId-1]})
+});
 
-// DESTROY a resource 
-animes.delete("/:animeId", (req, res)=> {
-    const {animeId} = req.params;
-    const [removedAnime] = animesArr.splice(animeId-1,1);
-    res.status(200).json({payload: removedAnime})
-})
-
+// DESTROY a resource
+animes.delete("/:animeId", async (req, res) => {
+  const { animeId } = req.params;
+  try {
+    const deletedAnime = await deleteOneAnime(animeId)
+    res.status(200).json({payload: deletedAnime});
+  } catch (error) {
+    res.status(404).json({payload: error})
+  }
+//   const [removedAnime] = animesArr.splice(animeId - 1, 1);
+//   res.status(200).json({ payload: removedAnime });
+});
 
 module.exports = animes;
--- queries/animes.js
+++ queries/animes.js
@@ -1,17 +1,65 @@
-const db = require("../db/dbConfig.js")
+const db = require("../db/dbConfig.js");
 
+const getAllAnimes = async () => {
+  try {
+    const animes = await db.any("SELECT * FROM animes");
+    return animes;
+  } catch (error) {
+    console.log("error", error);
+    return error;
+  }
+};
 
-const getAllAnimes = async() =>{
-    try {
-        const animes = await db.any("SELECT * FROM animes");
-        return animes;
-    }catch(error){
-        console.log("error", error)
-        return error
-    }
-}
+const getOneAnime = async (id) => {
+  try {
+    const anime = await db.one("SELECT * FROM animes WHERE id=$1", id);
+    return anime;
+  } catch (error) {
+    return error;
+  }
+};
 
+const createAnime = async ({ name, description }) => {
+  try {
+    const newAnime = await db.one(
+      "INSERT INTO animes (name, description) VALUES ($1, $2) RETURNING *",
+      [name, description]
+    );
+    return newAnime;
+  } catch (error) {
+    return error;
+  }
+};
+
+const updateOneAnime = async (id, body) => {
+  const { name, description } = body;
+  try {
+    const updatedAnime = await db.one(
+      "UPDATE animes SET name=$1, description=$2 WHERE id=$3 RETURNING *",
+      [name, description, id]
+    );
+    return updatedAnime;
+  } catch (error) {
+    return error;
+  }
+};
+
+const deleteOneAnime = async (id) => {
+  try {
+    const deletedAnime = await db.one(
+      "DELETE FROM animes WHERE id=$1 RETURNING *",
+      id
+    );
+    return deletedAnime;
+  } catch (error) {
+    return error;
+  }
+};
 
 module.exports = {
-    getAllAnimes
-}
+  getAllAnimes,
+  getOneAnime,
+  createAnime,
+  updateOneAnime,
+  deleteOneAnime,
+};
